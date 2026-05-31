64b340cca23004080717ec58bea16551f17a48c2
Aero <aero.windwalker@gmail.com>
2026-05-31 12:32:43 +0800

    chore: bump version

--- Formula/drift.rb
+++ Formula/drift.rb
@@ -1,7 +1,7 @@
 class Drift < Formula
   desc "Encrypted bidirectional file transfer over WebSocket with an embedded web UI"
   homepage "https://github.com/aeroxy/drift"
-  version "0.4.2"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
